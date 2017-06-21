67b76b7878d01fe1d3a503f22b030daa34076293
Chris Cannon <cannonc@gmail.com>
2017-06-21 12:44:13 -0700

    fixing paths and startup script

--- src/frontend/index.js
+++ src/frontend/index.js
@@ -1,13 +1,12 @@
 import { Router } from 'express';
-import webpack from 'webpack';
-import webpackDevMiddleware from 'webpack-dev-middleware';
-import webpackHotMiddleware from 'webpack-hot-middleware';
-import webpackConfig from '../../webpack.dev.config';
-import fs from 'fs';
 
 let api = Router();
 
 if(process.env.NODE_ENV === 'dev') {
+	const webpack = require('webpack');
+	const webpackDevMiddleware = require('webpack-dev-middleware');
+	const webpackHotMiddleware = require('webpack-hot-middleware');
+	const webpackConfig = require('../../webpack.dev.config');
 	let compiler = webpack(webpackConfig);
 
 	api.use(webpackDevMiddleware(compiler, {
