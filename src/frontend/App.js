b55c20a84e50b6e026bcea885609e38a68f1e297
Chris Cannon <ccan@uw.edu>
2018-04-12 15:06:19 -0700

    abstracting material theme

--- src/frontend/App.js
+++ src/frontend/App.js
@@ -2,29 +2,11 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import './css/style';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import { createMuiTheme } from 'material-ui/styles';
+import theme from './css/materialTheme';
 import Register from './Containers/Register';
 import Configure from './Containers/Configure';
 import PageWrapper from './Containers/PageWrapper';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
-
-
-const theme = createMuiTheme({
-    palette: {
-      primary: {
-        light: '#757ce8',
-        main: '#4b2e83',
-        dark: '#002884',
-        contrastText: '#fff',
-      },
-      secondary: {
-        light: '#ff7961',
-        main: '#f44336',
-        dark: '#ba000d',
-        contrastText: '#000',
-      },
-    },
-  });
 
 class App extends React.Component {
     constructor(props) {
---
+++ src/frontend/css/materialTheme.js
@@ -0,0 +1,18 @@
+import { createMuiTheme } from 'material-ui/styles';
+
+export default createMuiTheme({
+    palette: {
+      primary: {
+        light: '#757ce8',
+        main: '#4b2e83',
+        dark: '#002884',
+        contrastText: '#fff',
+      },
+      secondary: {
+        light: '#ff7961',
+        main: '#f44336',
+        dark: '#ba000d',
+        contrastText: '#000',
+      },
+    },
+  });
