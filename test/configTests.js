201138898bf97a1e675fb82e23b3119a2258f83a
Chris Cannon <ccan@uw.edu>
2018-05-03 12:47:50 -0700

    cleaning up testing a little

--- test/configTests.js
+++ test/configTests.js
@@ -1,65 +1,64 @@
-
+import should from 'should';
+import assert from 'assert';
 import configurator from '../src/backend/config/configurator';
 let config = configurator.get();
 
-var should = require('should');
-var assert = require('assert');
 
-describe('Array', function () {
-    describe('#BackendConfigTests()', function () {
-        it('config should have certificate', function () {
+describe('Array', () => {
+    describe('#BackendConfigTests()', () => {
+        it('config should have certificate', () => {
             config.should.have.property('certificate');
         });
-        it('config should have passphrase', function () {
+        it('config should have passphrase', () => {
             config.should.have.property('passphrase');
         });
-        it('config should have idcardBaseUrl', function () {
+        it('config should have idcardBaseUrl', () => {
             config.should.have.property('idcardBaseUrl');
         });
-        it('config should have pwsBaseUrl', function () {
+        it('config should have pwsBaseUrl', () => {
             config.should.have.property('pwsBaseUrl');
         });
-        it('config should have photoBaseUrl', function () {
+        it('config should have photoBaseUrl', () => {
             config.should.have.property('photoBaseUrl');
         });
-        it('config should have groupsBaseUrl', function () {
+        it('config should have groupsBaseUrl', () => {
             config.should.have.property('groupsBaseUrl');
         });
-        it('config should have groupsSearchUrl', function () {
+        it('config should have groupsSearchUrl', () => {
             config.should.have.property('groupsSearchUrl');
         });
-        it('config should have storeInGroupsWS', function () {
+        it('config should have storeInGroupsWS', () => {
             config.should.have.property('storeInGroupsWS');
         });
-        it('config should have groupAdmins', function () {
+        it('config should have groupAdmins', () => {
             config.should.have.property('groupAdmins');
         });
-        it('config should have groupDisplayName', function () {
+        it('config should have groupDisplayName', () => {
             config.should.have.property('groupDisplayName');
         });
-        it('config should have groupDescription', function () {
+        it('config should have groupDescription', () => {
             config.should.have.property('groupDescription');
         });
-        it('config should have groupNameBase', function () {
+        it('config should have groupNameBase', () => {
             config.should.have.property('groupNameBase');
         });
-        it('config should have groupNameLeaf', function () {
+        it('config should have groupNameLeaf', () => {
             config.should.have.property('groupNameLeaf');
         });
-        it('config should have enableRegisterAPI', function () {
+        it('config should have enableRegisterAPI', () => {
             config.should.have.property('enableRegisterAPI');
         });
-        it('config should have enableGroupsAPI', function () {
+        it('config should have enableGroupsAPI', () => {
             config.should.have.property('enableGroupsAPI');
         });
-        it('config should have enablePWSAPI', function () {
+        it('config should have enablePWSAPI', () => {
             config.should.have.property('enablePWSAPI');
         });
-        it('config should have enableIDCardAPI', function () {
+        it('config should have enableIDCardAPI', () => {
             config.should.have.property('enableIDCardAPI');
         });        
 
-        it('config should have enableConfigAPI', function () {
+        it('config should have enableConfigAPI', () => {
             config.should.have.property('enableConfigAPI');
         });
     });
