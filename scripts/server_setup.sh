e4d85d7ec51caa0d50417b34a1be254226473f22
Chris Cannon <ccan@uw.edu>
2018-05-08 11:05:46 -0700

    testing heroku setup

---
+++ scripts/server_setup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+if [ "$HEROKU_ENV" = "staging" ]; then
+  echo "setting up heroku environment"
+  cp config/config.json.example config/config.json
+  touch config/cert.pfx # make a dummy file for testing
+fi
