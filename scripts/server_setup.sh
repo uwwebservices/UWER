0713dc440be00cce57e5cf2afb26ab3c5b4b723a
Chris Cannon <38223469+ccan-uw@users.noreply.github.com>
2018-05-10 12:50:58 -0700

    Flows (#7)
    * package json script reorg
    
    * fixing broken script

--- scripts/server_setup.sh
+++ scripts/server_setup.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
-if [ "$HEROKU_ENV" = "staging" ]; then
-  echo "setting up heroku environment"
+if [ ! -e config/config.json ]
+then
+  echo "Creating default config.json"
   cp config/config.json.example config/config.json
-  touch config/cert.pfx # make a dummy file for testing
+fi
+if [ ! -e config/cert.pfx ]
+then
+  echo "Creating dummy cert.pfx"
+  touch config/cert.pfx
 fi
