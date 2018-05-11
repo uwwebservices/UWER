a65ca12db07ef2123bbd299b82068b351200ecbd
Chris Cannon <38223469+ccan-uw@users.noreply.github.com>
2018-05-11 14:25:49 -0700

    Fix Config for Docker/Travis/Heroku (#8)
    * moving example config to another folder
    
    * copying config examples into dockerfile

--- scripts/server_setup.sh
+++ scripts/server_setup.sh
@@ -2,10 +2,15 @@
 if [ ! -e config/config.json ]
 then
   echo "Creating default config.json"
-  cp config/config.json.example config/config.json
+  cp config_base/config.json config/config.json
 fi
 if [ ! -e config/cert.pfx ]
 then
-  echo "Creating dummy cert.pfx"
-  touch config/cert.pfx
+  echo "Copying dummy cert.pfx"
+  cp config_base/cert.pfx config/cert.pfx
+fi
+if [ ! -e config/uwca.pem ]
+then
+  echo "Coping UWCA Cert"
+  cp config_base/uwca.pem config/uwca.pem
 fi
