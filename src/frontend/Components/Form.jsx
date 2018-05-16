a8e214abbcb15569026e6a5e9a1731fcf900671e
Chris Cannon <ccan@uw.edu>
2018-05-16 15:52:16 -0700

    one more redux api rewrite

--- src/frontend/Components/Form.jsx
+++ src/frontend/Components/Form.jsx
@@ -3,39 +3,21 @@ import TextField from 'material-ui/TextField';
 import Button from 'material-ui/Button';
 
 export default class Form extends React.Component {
-    constructor (props) {
-        super(props);
-        this.state = { card: ""};
-    }
-    
-    registerUser = e => {
+    registerUser = async e => {
         e.preventDefault();
-        if(this.state.card) {
-            let cardnum = this.state.card;
-            if(cardnum[0] === ';') {
-                cardnum = cardnum.slice(1, -1);
-            }
-            this.props.addUser(cardnum);
-
-
-            // fetch('/api/register/' + cardnum + '?verbose=true', {
-            //     method: 'PUT'
-            // }).then(res => res.json())
-            // .then(json => {
-            //     this.props.addUser(json);
-            // })
-            this.setState({ card: ""});
+        let identifier = e.target.registerCard.value;
+        if(identifier.length) {
+            // if identifier is a magstrip, remove the semicolon
+            identifier = identifier[0] === ';' ? identifier.slice(1, -1) : identifier;
+            await this.props.addUser(identifier);
+            document.getElementById("registerCard").value = "";
         }
-    };
-    
-    updateCard = e => {
-        this.setState({ card: e.target.value });
     };
 
     render() {
         return (
             <form className="form" onSubmit={this.registerUser}>
-                <TextField type="text" placeholder="magstrip/rfid/netid" id="registerCard" value={this.state.card} onChange={this.updateCard} />
+                <TextField type="text" name="registerCard" id="registerCard" placeholder="magstrip/rfid/netid"  />
                 <Button variant="raised" color="primary" type="submit">Register</Button>
             </form>
         )
--- src/frontend/Store.js
+++ src/frontend/Store.js
@@ -1,12 +1,12 @@
 import { createStore, applyMiddleware  } from 'redux';
-import Reducer from './Reducers';
+import RootReducer from './Reducers';
 import thunk from 'redux-thunk';
 import { createLogger } from 'redux-logger'
 
 const loggerMiddleware = createLogger();
 
 export default createStore(
-    Reducer, 
+    RootReducer, 
     applyMiddleware(
         thunk, 
         loggerMiddleware
