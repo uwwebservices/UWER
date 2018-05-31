ab13151b8f96036147deb11efed5a67533db69fe
Chris Cannon <ccan@uw.edu>
2018-05-31 13:31:52 -0700

    update passport again

--- src/backend/controllers/frontend.js
+++ src/backend/controllers/frontend.js
@@ -5,7 +5,7 @@ import saml from 'passport-saml';
 
 let samlStrategy = new saml.Strategy(
 	{
-    path: 'https://idcard-poc-staging.herokuapp.com/login/callback',
+    callbackUrl: 'https://idcard-poc-staging.herokuapp.com/login/callback',
     entryPoint: 'https://idp.u.washington.edu/idp/profile/SAML2/Redirect/SSO',
 		issuer: 'http://ccan.cac.washington.edu/idcard',
 		identifierFormat: "urn:oasis:names:tc:SAML:1.1:nameid-format:unspecified"
