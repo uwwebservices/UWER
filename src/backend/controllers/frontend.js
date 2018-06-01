4d5aebbec8a992568cff9f5bd9dcb850590f935d
Chris Cannon <ccan@uw.edu>
2018-06-01 11:56:01 -0700

    moving logs

--- src/backend/controllers/frontend.js
+++ src/backend/controllers/frontend.js
@@ -46,10 +46,6 @@ if(process.env.NODE_ENV === 'development') {
 		reload: true
 	}))
 	app.get('*', (req, res, next) => {
-		console.log("req.user", req.user);
-		console.log("req.session", req.session);
-		console.log("req.session.passport", req.session.passport);
-		
 		var filename = path.join(compiler.outputPath,'index.html');
 		compiler.outputFileSystem.readFile(filename, function(err, result){
 			if (err) {
@@ -64,6 +60,9 @@ if(process.env.NODE_ENV === 'development') {
 
 if(process.env.NODE_ENV === 'production') {
 	app.get(['/', '/config'], (req, res) => {
+		console.log("req.user", req.user);
+		console.log("req.session", req.session);
+		console.log("req.session.passport", req.session.passport);
 		res.sendFile(path.resolve(__dirname, '..', '..', 'index.html'));
  });
 }
