cabf630e0b50e3f8802a6be4ee7ef0992b51db92
Chris Cannon <ccan@uw.edu>
2018-06-01 11:33:19 -0700

    readding saml verification strategy

--- src/backend/index.js
+++ src/backend/index.js
@@ -35,7 +35,14 @@ let samlStrategy = new saml.Strategy(
 		entryPoint: 'https://idp.u.washington.edu/idp/profile/SAML2/Redirect/SSO',
 		issuer: 'http://ccan.cac.washington.edu/idcard',
 		identifierFormat: "urn:oasis:names:tc:SAML:1.1:nameid-format:unspecified"
-	});
+	},
+	function(profile, done) {
+		console.log("OMG PROFILE", profile);
+		return done(null, {
+			UWNetID: profile.nameID,
+			DisplayName: profile["urn:oid:2.16.840.1.113730.3.1.241"]
+		})
+});
 
 passport.serializeUser(function(user, done) {
 	console.log("SERIALIZE USER", user);
