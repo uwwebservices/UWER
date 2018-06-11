bd08ae1055a00e73ad5946ed7d1798324e7b9124
Chris Cannon <ccan@uw.edu>
2018-06-11 16:35:58 -0700

    fixing path

--- bin/ecs_deploy.sh
+++ bin/ecs_deploy.sh
@@ -5,7 +5,7 @@ if [ -z "$TRAVIS_PULL_REQUEST" ] || [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
 # Deploy only if we're testing the master branch
 if [ "$TRAVIS_BRANCH" == "master" ]; then
 echo "Deploying $TRAVIS_BRANCH on $TASK_DEFINITION"
-./bin/ecs-deploy -c $TASK_DEFINITION -n $SERVICE -i $REMOTE_IMAGE_URL:$TRAVIS_BRANCH
+bash ./bin/ecs-deploy.sh -c $TASK_DEFINITION -n $SERVICE -i $REMOTE_IMAGE_URL:$TRAVIS_BRANCH
 else
 echo "Skipping deploy because it's not an allowed branch"
 fi
