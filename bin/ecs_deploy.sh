0bbc9e9d451cf37322d6c869c5fc898a6c5c2587
Chris Cannon <ccan@uw.edu>
2018-06-12 10:08:14 -0700

    testing cd

--- bin/ecs_deploy.sh
+++ bin/ecs_deploy.sh
@@ -2,13 +2,13 @@
 # Deploy only if it's not a pull request
 echo "DEPLOYING TO ECS!"
 if [ -z "$TRAVIS_PULL_REQUEST" ] || [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
-# Deploy only if we're testing the master branch
-if [ "$TRAVIS_BRANCH" == "master" ]; then
-echo "Deploying $TRAVIS_BRANCH on $TASK_DEFINITION"
-bash ./bin/ecs-deploy.sh -d $TASK_DEFINITION -n $SERVICE -i $REMOTE_IMAGE_URL:$TRAVIS_BRANCH
+  # Deploy only if we're testing the master branch
+  if [ "$TRAVIS_BRANCH" == "master" ]; then
+    echo "Deploying $TRAVIS_BRANCH on $TASK_DEFINITION"
+    bash ./bin/ecs-deploy.sh -d $TASK_DEFINITION -c $CLUSTER -n $SERVICE -i $REMOTE_IMAGE_URL:latest
+  else
+    echo "Skipping deploy because it's not an allowed branch"
+  fi
 else
-echo "Skipping deploy because it's not an allowed branch"
-fi
-else
-echo "Skipping deploy because it's a PR"
+  echo "Skipping deploy because it's a PR"
 fi
