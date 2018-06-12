5d13fac7658408e263a26ec2df9aac9b43bd36cd
Chris Cannon <ccan@uw.edu>
2018-06-12 13:12:12 -0700

    cleanup

--- bin/ecs_deploy.sh
+++ bin/ecs_deploy.sh
@@ -1,6 +1,5 @@
 #! /bin/bash
 # Deploy only if it's not a pull request
-echo "DEPLOYING TO ECS!"
 if [ -z "$TRAVIS_PULL_REQUEST" ] || [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
   # Deploy only if we're testing the master branch
   if [ "$TRAVIS_BRANCH" == "master" ]; then
