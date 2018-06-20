883ce173fbdf20ea84eff3bfc4466b0f45134a8e
Chris Cannon <ccan@uw.edu>
2018-06-20 15:40:45 -0700

    testing develop cicd

--- bin/docker-push.sh
+++ bin/docker-push.sh
@@ -1,8 +1,9 @@
 #! /bin/bash
 # Push only if it's not a pull request
 if [ -z "$TRAVIS_PULL_REQUEST" ] || [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
-  # Push only if we're testing the master or devlop branch
+  # Push only if we're testing the master branch
   if [ "$TRAVIS_BRANCH" == "master" ] || [ "$TRAVIS_BRANCH" == "develop" ]; then
+
     # This is needed to login on AWS and push the image on ECR
     # Change it accordingly to your docker repo
     pip install --user awscli
@@ -15,13 +16,13 @@ if [ -z "$TRAVIS_PULL_REQUEST" ] || [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
     esac
 
     # Build and push
-    docker build -t $IMAGE_NAME .
+    docker build -t $IMAGE_NAME:latest .
     echo "Pushing $IMAGE_NAME:$TAG"
-    docker tag "$IMAGE_NAME:$TAG" "$REMOTE_IMAGE_URL:$TAG"
+    docker tag $IMAGE_NAME:latest "$REMOTE_IMAGE_URL:$TAG"
     docker push "$REMOTE_IMAGE_URL:$TAG"
     echo "Pushed $IMAGE_NAME:$TAG"
   else
-    echo "Skipping deploy because branch is not 'develop' or 'master'"
+    echo "Skipping deploy because branch is not 'master'"
   fi
 else
   echo "Skipping deploy because it's a pull request"
--- bin/ews-deploy.sh
+++ bin/ews-deploy.sh
@@ -2,15 +2,20 @@
 # Deploy only if it's not a pull request
 if [ -z "$TRAVIS_PULL_REQUEST" ] || [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
   # Deploy only if we're testing the master branch
-  if [ "$TRAVIS_BRANCH" == "master" ]; then
-    echo "Deploying..."
-    echo "Branch: $TRAVIS_BRANCH, TASK: $TASK_DEFINITION, Cluster: $CLUSTER, Service: $SERVICE, Image: $REMOTE_IMAGE_URL:latest"
+  if [ "$TRAVIS_BRANCH" == "master" ] || [ "$TRAVIS_BRANCH" == "develop" ]; then
+    
+    case "$TRAVIS_BRANCH" in
+      "master") TAG="latest";TASK_DEF=$TASK_DEFINITION ;;
+      "develop") TAG="develop";TASK_DEF=$DEV_TASK_DEFINITION ;;
+    esac
+
+    echo "Branch: $TRAVIS_BRANCH, TASK: $TASK_DEF, Cluster: $CLUSTER, Service: $SERVICE, Image: $REMOTE_IMAGE_URL:$TAG"
 
     # Use this for deploying to a service
     #bash ./bin/ecs-deploy.sh -c $CLUSTER -n $SERVICE -i $REMOTE_IMAGE_URL:latest --max-definitions 5 -v
     
     # use this to just update the task definition (manual deploy)
-    bash ./bin/ecs-deploy.sh -c $CLUSTER -d $TASK_DEFINITION -i $REMOTE_IMAGE_URL:latest --max-definitions 5
+    bash ./bin/ecs-deploy.sh -c $CLUSTER -d $TASK_DEF -i $REMOTE_IMAGE_URL:$TAG --max-definitions 5
     
   else
     echo "Skipping deploy because it's not an allowed branch"
