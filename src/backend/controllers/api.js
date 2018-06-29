78d69f54da1b49744945bcb4f0d8faab557cffaa
gvnmccld <cannonc@gmail.com>
2018-06-29 13:34:01 -0400

    clearing passport cookie too

--- src/backend/controllers/api.js
+++ src/backend/controllers/api.js
@@ -23,6 +23,7 @@ api.get(API.GetToken, async (req, res) => {
 api.get(API.Logout, (req,res) => {
 	req.logout();
 	req.session.destroy();
+	res.clearCookie('connect.sid');
 	res.sendStatus(200);
 });
 
