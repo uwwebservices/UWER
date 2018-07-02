6a5e3650086181c7f6cc9832411a9510c3b6e5fd
gvnmccld <cannonc@gmail.com>
2018-07-02 14:15:31 -0400

    fix logout bug

--- src/backend/utils/helpers.js
+++ src/backend/utils/helpers.js
@@ -59,15 +59,14 @@ export const getAuthToken = (req, uriEncode = true) => {
 }
 
 export const verifyAuthToken = req => {
-	console.log("verifying token", req.session.token, req.body.token);
 	if(!req.session.token && !req.body.token) { return false; }
 	if(!req.session.token && req.body.token) { req.session.token = decodeURIComponent(req.body.token); }
-	console.log("loaded token", req.session.token, req.body.token);
+
 	let passphrase = process.env.SessionKey || "development";
 	let payload = AES.decrypt(req.session.token, passphrase).toString(enc.Utf8);
-	console.log(payload);
+
 	payload = JSON.parse(payload);
 	req.session.user = payload.user;
-	console.log("Token expired?", payload.expiry, (new Date()).getTime(), payload.expiry > (new Date()).getTime())
+
 	return payload.expiry > (new Date()).getTime();
 }
