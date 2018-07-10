5c95b9e6e0dbb946c43e2e20a5224c31cbb0bb06
gvnmccld <cannonc@gmail.com>
2018-07-10 12:36:42 -0400

    tweaking deploy script:

--- bin/ews-deploy.sh
+++ bin/ews-deploy.sh
@@ -11,12 +11,12 @@ if [ -z "$TRAVIS_PULL_REQUEST" ] || [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
 
     echo "Branch: $TRAVIS_BRANCH, TASK: $TASK_DEF, Cluster: $CLUSTER, Service: $SERVICE, Image: $REMOTE_IMAGE_URL:$TAG"
 
-    # Use this for deploying to a service
+    # Use this for deploying to a service - max-definitions only works with a service
     #bash ./bin/ecs-deploy.sh -c $CLUSTER -n $SERVICE -i $REMOTE_IMAGE_URL:latest --max-definitions 5 -v
     
     # use this to just update the task definition (manual deploy)
-    bash ./bin/ecs-deploy.sh -c $CLUSTER -d $TASK_DEF -i $REMOTE_IMAGE_URL:$TAG --max-definitions 1 -v
-    
+    bash ./bin/ecs-deploy.sh -c $CLUSTER -d $TASK_DEF -i $REMOTE_IMAGE_URL:$TAG 
+
   else
     echo "Skipping deploy because it's not an allowed branch"
   fi
