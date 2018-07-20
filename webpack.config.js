34ac8da09e9140e97d8e1d3dbf7f0e20ec0aa2b6
ccan_uw <ccan@uw.edu>
2018-07-20 11:31:23 -0400

    test

--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,7 @@ module.exports = {
   entry: './src/frontend/App.js',
   output: {
     path: path.resolve(__dirname + '/dist/'),
-    publicPath: process.env.BASE_URL || "/",
+    publicPath: "/idcard-eval/",
     filename: 'assets/bundle.js'
   },
   optimization: {
