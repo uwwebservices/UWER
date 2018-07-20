8d830de35f00df6420b592451c0d17078311a903
ccan_uw <ccan@uw.edu>
2018-07-20 11:55:57 -0400

    testing react router fix

--- src/frontend/App.js
+++ src/frontend/App.js
@@ -25,7 +25,7 @@ const PorousRoute = function ({ children, ...props }) {
 class App extends React.Component {
     render () {
         return (
-            <Router>
+            <Router basename="/idcard-eval/">
                 <Provider store={store}>
                     <MuiThemeProvider theme={theme}>
                             <Switch>
