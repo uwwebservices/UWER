b2b380ea872bba56764db9708631088002f0c53f
ccan_uw <ccan@uw.edu>
2018-08-02 10:28:20 -0400

    fixing deploy script

--- bin/ews-deploy.sh
+++ bin/ews-deploy.sh
@@ -12,7 +12,7 @@ if [ -z "$TRAVIS_PULL_REQUEST" ] || [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
     echo "Branch: $TRAVIS_BRANCH, TASK: $TASK_DEF, Cluster: $CLUSTER, Service: $SERVICE, Image: $REMOTE_IMAGE_URL:$TAG"
 
     # Use this for deploying to a service - max-definitions only works with a service
-    bash ./bin/ecs-deploy.sh -c $CLUSTER -n $SERVICE -d $TASK_DEF -i $REMOTE_IMAGE_URL:$TAG --max-definitions 5 -v
+    bash ./bin/ecs-deploy.sh -c $CLUSTER -n $SERVICE -i $REMOTE_IMAGE_URL:$TAG --max-definitions 5 -v
     
     # use this to just update the task definition (manual deploy)
     #bash ./bin/ecs-deploy.sh -c $CLUSTER -d $TASK_DEF -i $REMOTE_IMAGE_URL:$TAG 
