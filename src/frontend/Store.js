cb639ad08d4e67e804cf542ed5bb7e6be1f7c3f0
ccan_uw <ccan@uw.edu>
2018-08-22 12:25:52 -0400

    more logging

--- src/frontend/Store.js
+++ src/frontend/Store.js
@@ -6,6 +6,7 @@ import { createLogger } from 'redux-logger'
 let middleware = [thunk];
 
 console.log("REDUX_LOGGING", process.env.REDUX_LOGGING);
+console.log("NODE_ENV", process.env.NODE_ENV);
 if(process.env.NODE_ENV === 'development' || process.env.REDUX_LOGGING) {
     const logger = createLogger();
     middleware.push(logger);
