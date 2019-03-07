99aaa573f61dab6309aa6f7d118b643699b6d5f7
ccan_uw <ccan@uw.edu>
2019-03-07 10:19:27 -0500

    initial stab at badges

--- src/backend/models/pwsModel.js
+++ src/backend/models/pwsModel.js
@@ -6,46 +6,47 @@ const PASSPHRASEFILE = process.env.PASSPHRASEFILE;
 const PWSBASEURL = process.env.PWSBASEURL;
 
 const options = {
-    method: 'GET',
-    url: "",
-    agentOptions: {
-        pfx: fs.readFileSync(CERTIFICATEFILE),
-        passphrase: fs.readFileSync(PASSPHRASEFILE, { encoding: 'utf8'}).toString(), // WHY IS THERE A \n after?! fix this.
-        securityOptions: 'SSL_OP_NO_SSLv3' 
-    },
-    json: true
+  method: 'GET',
+  url: '',
+  agentOptions: {
+    pfx: fs.readFileSync(CERTIFICATEFILE),
+    passphrase: fs.readFileSync(PASSPHRASEFILE, { encoding: 'utf8' }).toString(),
+    securityOptions: 'SSL_OP_NO_SSLv3'
+  },
+  json: true
 };
 
-const defaultWhiteList = ["DisplayName", "UWNetID", "UWRegID"];
+const defaultWhiteList = ['DisplayName', 'UWNetID', 'UWRegID', 'EduPersonAffiliations'];
 const FilterPWSModel = (model, whitelist = defaultWhiteList) => {
-    return Object.keys(model)
-        .filter(key => whitelist.includes(key))
-        .reduce((obj, key) => {
-            obj[key] = model[key];
-            return obj;
-        }, {});
-}
+  return Object.keys(model)
+    .filter(key => whitelist.includes(key))
+    .reduce((obj, key) => {
+      obj[key] = model[key];
+      return obj;
+    }, {});
+};
 
 const PWS = {
-    async Get(identifier, whitelist) {
-        let res;
-        let opts = Object.assign({}, options, { 
-            url: `${PWSBASEURL}/${identifier}/full.json`,
-        });
-        try {
-            res = await rp(opts);
-        } catch (ex) {
-            res = { "UWNetID": "User Registered", "DisplayName": "Details Not Found", "UWRegID": ""};
-        }
-        return FilterPWSModel(res, whitelist);
-    },
-    async GetMany(memberList, whitelist) {
-        let members = [];
-        for(let mem of memberList) {
-            members.push(this.Get(mem.id, whitelist));
-        }
-        return await Promise.all(members);
+  async Get(identifier, whitelist) {
+    let res;
+    let opts = Object.assign({}, options, {
+      url: `${PWSBASEURL}/${identifier}/full.json`
+    });
+    try {
+      res = await rp(opts);
+      console.log(res);
+    } catch (ex) {
+      res = { UWNetID: 'User Registered', DisplayName: 'Details Not Found', UWRegID: '' };
     }
-}
+    return FilterPWSModel(res, whitelist);
+  },
+  async GetMany(memberList, whitelist) {
+    let members = [];
+    for (let mem of memberList) {
+      members.push(this.Get(mem.id, whitelist));
+    }
+    return await Promise.all(members);
+  }
+};
 
 export default PWS;
--- src/frontend/Components/Members.jsx
+++ src/frontend/Components/Members.jsx
@@ -4,56 +4,75 @@ import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
 import ConfirmModal from 'Components/ConfirmModal';
+import Chip from '@material-ui/core/Chip';
 import FA from 'react-fontawesome';
 
 export default class Test extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {loadingUsers: false};
+  constructor(props) {
+    super(props);
+    this.state = { loadingUsers: false };
+  }
+  removeUser = netid => {
+    this.props.removeUser(this.props.group, netid);
+    document.getElementById('registerCard').focus();
+  };
+  reload = async () => {
+    if (this.props.group) {
+      this.setState({ loadingUsers: true });
+      await this.props.reloadUsers(this.props.group);
+      this.setState({ loadingUsers: false });
     }
-    removeUser = netid => {
-        this.props.removeUser(this.props.group, netid);
-        document.getElementById("registerCard").focus();
-    }
-    reload = async () => {
-        if(this.props.group){
-            this.setState({loadingUsers: true});
-            await this.props.reloadUsers(this.props.group);
-            this.setState({loadingUsers: false});
-        }
-    }
-    render() {
-        const listItems = this.props.members.map(mem => {
-            let showDelete = (this.props.authenticated || this.props.development) && !mem.deleting && !mem.loading;
-            return (
-                <ListItem
-                    key={(mem.UWNetID || mem.identifier) + (Math.floor(Math.random() * 100)).toString()}
-                    className={mem.deleting ? "memberDeleting" : ""}>
-                    <Avatar src={mem.Base64Image} />
-                    <ListItemText primary={mem.loading ? "Loading..." : mem.UWNetID} secondary={mem.DisplayName} />
-                    { (mem.deleting || mem.loading) && <span className="loadSpinner"><FA name="spinner" spin={true} size="2x" /></span> }
-                    { showDelete && (
-                        <ConfirmModal openButtonIcon="remove" openButtonText="" 
-                            openButtonVariant="fab" openButtonFabMini={true} 
-                            confirmCallback={() => this.removeUser(mem.UWNetID)} 
-                            dialogContent={`Are you sure you want to remove ${mem.UWNetID} from ${this.props.group.replace(this.props.groupNameBase, "").replace(/-/g, ' ')}?`} 
-                            dialogTitle={`Remove User?`} 
-                        />
-                    )}
-                </ListItem> 
-            )
-        })
-        return (                   
-                <div className="memberList">    
-                    { this.props.members.length > 0 && 
-                        <div>
-                        <h2>Registered Participants <FA name="refresh" onClick={this.reload} spin={this.state.loadingUsers}/></h2>
-                        <List>
-                            {listItems}
-                        </List>
-                        </div>
-                    }
-                </div>         
-        )        
-    }
+  };
+  render() {
+    const listItems = this.props.members.map(mem => {
+      let showDelete = (this.props.authenticated || this.props.development) && !mem.deleting && !mem.loading;
+      let isStudent = mem.EduPersonAffiliations && mem.EduPersonAffiliations.indexOf('student') > -1;
+      let isEmployee = mem.EduPersonAffiliations && mem.EduPersonAffiliations.indexOf('employee') > -1;
+      let isAlum = mem.EduPersonAffiliations && mem.EduPersonAffiliations.indexOf('alum') > -1;
+      let badges = !mem.loading && (
+        <span>
+          {mem.UWNetID}
+          {isStudent && <Chip label="student" />}
+          {isEmployee && <Chip label="employee" />}
+          {isAlum && <Chip label="alum" />}
+        </span>
+      );
+      return (
+        <ListItem key={(mem.UWNetID || mem.identifier) + Math.floor(Math.random() * 100).toString()} className={mem.deleting ? 'memberDeleting' : ''}>
+          <Avatar src={mem.Base64Image} />
+          <ListItemText primary={badges || 'Loading...'} secondary={mem.DisplayName} />
+          {(mem.deleting || mem.loading) && (
+            <span className="loadSpinner">
+              <FA name="spinner" spin={true} size="2x" />
+            </span>
+          )}
+          {showDelete && (
+            <ConfirmModal
+              openButtonIcon="remove"
+              openButtonText=""
+              openButtonVariant="fab"
+              openButtonFabMini={true}
+              confirmCallback={() => this.removeUser(mem.UWNetID)}
+              dialogContent={`Are you sure you want to remove ${mem.UWNetID} from ${this.props.group
+                .replace(this.props.groupNameBase, '')
+                .replace(/-/g, ' ')}?`}
+              dialogTitle={`Remove User?`}
+            />
+          )}
+        </ListItem>
+      );
+    });
+    return (
+      <div className="memberList">
+        {this.props.members.length > 0 && (
+          <div>
+            <h2>
+              Registered Participants <FA name="refresh" onClick={this.reload} spin={this.state.loadingUsers} />
+            </h2>
+            <List>{listItems}</List>
+          </div>
+        )}
+      </div>
+    );
+  }
 }
