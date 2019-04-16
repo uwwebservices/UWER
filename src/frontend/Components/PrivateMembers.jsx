f031e258543b68212223d3143d51af91e5565e8f
Dustin Brewer <mannkind@uw.edu>
2019-04-16 10:16:01 -0700

    Simplified PrivateMembers
    If the confidential flag is set, the Register container will use the PrivateMembers component.
    However, the API does not return members if the group is confidential meaning nothing is
    ever rendered.
    
    One exception to "nothing is ever rendered" is when the the temporary/default user is
    added to the list during a registration attempt.
    
    *This* is the reason the component was kept; could probably be simplied even more if desired.

--- src/frontend/Components/PrivateMembers.jsx
+++ src/frontend/Components/PrivateMembers.jsx
@@ -8,39 +8,19 @@ import DefaultUser from 'Assets/defaultUser';
 export default class Test extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { loadingUsers: false };
   }
-  reload = async () => {
-    if (this.props.group) {
-      this.setState({ loadingUsers: true });
-      await this.props.reloadUsers(this.props.group);
-      this.setState({ loadingUsers: false });
-    }
-  };
+
   render() {
-    return this.props.members.map(mem => {
-      let memberKey = mem.UWNetID || mem.identifier || Math.random().toString(36);
-      let loadRealImg = true;
-      const loadImgOnce = e => {
-        if (loadRealImg) {
-          loadRealImg = false;
-          e.target.src = mem.Base64Image;
-        }
-      };
-      const errImg = e => {
-        e.target.src = DefaultUser;
-      };
-      return (
-        <ListItem key={memberKey}>
-          <Avatar onLoad={loadImgOnce} onError={errImg} src={DefaultUser} />
-          <ListItemText primary={mem.loading ? 'Loading...' : mem.UWNetID} secondary={mem.DisplayName} />
-          {(mem.deleting || mem.loading) && (
-            <span className="loadSpinner">
-              <FA name="spinner" spin={true} size="2x" />
-            </span>
-          )}
-        </ListItem>
-      );
-    });
+    return this.props.members && this.props.members.length != 0 ? (
+      <ListItem key={Math.random().toString(36)}>
+        <Avatar src={DefaultUser} />
+        <ListItemText primary="Loading..." />
+        <span className="loadSpinner">
+          <FA name="spinner" spin={true} size="2x" />
+        </span>
+      </ListItem>
+    ) : (
+      <ListItem />
+    );
   }
 }
