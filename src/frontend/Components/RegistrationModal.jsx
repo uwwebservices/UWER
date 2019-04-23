544f7b230f1d4a22c69966326ad26b60a3faa22d
Dustin Brewer <mannkind@uw.edu>
2019-04-23 12:14:56 -0700

    Allow 'back' button on Start Registration Modal to work?
    This might not work.
    This might be stupid.
    This might need to be reverted.
    
    🤷

--- src/frontend/Components/RegistrationModal.jsx
+++ src/frontend/Components/RegistrationModal.jsx
@@ -2,6 +2,12 @@ import React from 'react';
 import ContentModal from 'Components/ContentModal';
 
 class RegistrationModal extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showLogout: false
+    };
+  }
   backToConfig() {
     if (process.env.NODE_ENV !== 'development') {
       window.location = '/login?returnUrl=/config';
@@ -15,14 +21,25 @@ class RegistrationModal extends React.Component {
       approveButtonText: 'Start Registration',
       openButtonText: 'Registration Mode',
       disableBackdropClick: true,
-      cancelCallback: this.backToConfig
+      cancelCallback: this.backToConfig,
+      ...this.props
+    };
+    modalOpts.confirmCallback = async () => {
+      this.setState({ showLogout: true });
+
+      // Wait until the logout works OR we've tried too many times
+      // This should allow the iFrame to load
+      let count = 0;
+      let maxCount = 15;
+      while (count < maxCount) {
+        count += 1;
+        await new Promise(resolve => setTimeout(resolve, 1000));
+      }
     };
 
     return (
-      <ContentModal {...Object.assign({}, modalOpts, this.props)}>
-        <div>
-          <iframe src="https://idp.u.washington.edu/idp/profile/Logout" height="335px" width="450px" />
-        </div>
+      <ContentModal {...modalOpts}>
+        <div>{this.state.showLogout ? <iframe onLoad={this.props.confirmCallback} src="https://idp.u.washington.edu/idp/profile/Logout" height="335px" width="450px" /> : <p>Are you sure that you want to begin registration?</p>}</div>
       </ContentModal>
     );
   }
