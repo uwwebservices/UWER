72a2ddca61e7bc20cb11868b758093c67117286d
Dustin Brewer <mannkind@uw.edu>
2019-10-22 16:42:28 +0000

    Base group removal typo

--- src/backend/models/groupModel.js
+++ src/backend/models/groupModel.js
@@ -109,7 +109,7 @@ const Groups = {
 
     // GWS sometimes returns the base group in the results with this type of query, lets remove that
     const basegroupidx = groupList.indexOf(group.slice(0, -1));
-    groupList.splice(groupList.indexOf(basegroupidx), 1);
+    groupList.splice(basegroupidx, 1);
 
     if (verbose) {
       let verboseGroups = [];
